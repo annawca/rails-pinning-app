43f2b0f455c299fde0715489862f523c384fc9d8
a <cha.yuki@gmail.com>
2016-04-09 15:01:24 -0700

    edit rspec successfully working and added

--- spec/requests/requests_spec.rb
+++ spec/requests/requests_spec.rb
@@ -1,5 +1,13 @@
 require "spec_helper"
 
 RSpec.describe "Our Application Routes" do
-  
+  	describe "GET /pins/name-:slug" do
+  		
+  		it 'populates the @pin variable with appropriate pin' do
+  			pin = Pin.first
+  			get "/pins/name-#{pin.slug}"
+  			expect(assigns[:pin]).to eq(Pin.find(1))
+  		end
+
+  	end
 end
