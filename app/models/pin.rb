35bbdb7b6163acfa6f77d59dd9cd56ec7e9b9984
a <cha.yuki@gmail.com>
2016-04-13 21:47:03 -0700

    change the default image to asset_path inside model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,6 @@ class Pin < ActiveRecord::Base
   belongs_to :category, inverse_of: :pin
   validates_presence_of :title, :url, :slug, :text, :category_id
   validates_uniqueness_of :slug
-  has_attached_file :image, styles: {medium: "300x300>", thumb: "80x80>"}, :default_url => "/assets/missing_:style.png"
+  has_attached_file :image, styles: {medium: "300x300>", thumb: "80x80>"},default_url: ->(attachment) { ActionController::Base.helpers.asset_path("missing_:style.png") }
   validates_attachment :image, :content_type => { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] } unless Rails.env.test?
 end
